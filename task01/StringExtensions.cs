0c7a695a96b8f1bc26b9ded8a44c286f13a17ecd
RuSSiCH <vladkarmomich@mail.ru>
2026-06-25 22:17:09 +0600

    feat: task-1 add IsPalindrome method and test

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if(string.IsNullOrEmpty(input)){
+            return false;
+        }
+        string low=input.ToLower();
+        string clean="";
+        foreach(char c in low){
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c)){
+                clean+=c;
+            }
+        }
+        if(string.IsNullOrEmpty(input)){
+            return false;
+        }
+        string revers="";
+        for (int i=clean.Length-1;i>=0;i--){
+            revers+=clean[i];
+        }
+        return clean==revers;
+
+    }
+
+}
