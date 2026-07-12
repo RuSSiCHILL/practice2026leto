c7f12f5acb6b34145598d44ded73f6cfc980ca52
RuSSiCH <vladkarmomich@mail.ru>
2026-07-12 21:41:54 +0600

    task08: dynamic library

---
+++ task08/CommandRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+var dllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileSystemCommands.dll");
+
+if (!File.Exists(dllPath))
+{
+    Console.WriteLine("DLL не найдена.");
+    return;
+}
+
+var assembly = Assembly.LoadFrom(dllPath);
+
+foreach (var type in assembly.GetTypes().Where(t => typeof(ICommand).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract))
+{
+    var command = (ICommand)Activator.CreateInstance(type);
+    command.Execute();
+}
---
+++ task08/FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class FindFilesCommand : ICommand
+{
+    private readonly string _directoryPath;
+    private readonly string _mask;
+    public List<string> FoundFiles { get; private set; }
+
+    public FindFilesCommand(string directoryPath, string mask)
+    {
+        _directoryPath = directoryPath;
+        _mask = mask;
+    }
+
+    public void Execute()
+    {
+        FoundFiles = new List<string>(Directory.GetFiles(_directoryPath, _mask, SearchOption.AllDirectories));
+        Console.WriteLine("Найдено файлов: " + FoundFiles.Count);
+    }
+}
