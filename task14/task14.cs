8fdfa70256fe158684af0d1d4ac68f0e784e18d5
RuSSiCH <vladkarmomich@mail.ru>
2026-07-15 19:57:54 +0600

    task15

--- task14/task14.cs
+++ task14/task14.cs
@@ -3,6 +3,18 @@ using System.Threading;
 
 public class DefiniteIntegral
 {
+    public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+    {
+        double result = 0;
+        int stepsCount = (int)((b - a) / step);
+        for (int i = 0; i < stepsCount; i++)
+        {
+            double x1 = a + i * step;
+            double x2 = x1 + step;
+            result += (function(x1) + function(x2)) / 2 * step;
+        }
+        return result;
+    }
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
     {
         if (threadsNumber < 1)
@@ -28,7 +40,6 @@ public class DefiniteIntegral
                 while (Interlocked.CompareExchange(ref totalResult, sum, original) != original);
                 barrier.SignalAndWait();
             });
-
             threads[i].Start();
         }
         foreach (var thread in threads)
